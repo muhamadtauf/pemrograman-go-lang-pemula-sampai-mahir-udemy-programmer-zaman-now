8b59c41483d0ebb8d23179a01503272d93b09d7a
Muhammad Taufik <muhamadtauf@gmail.com>
2023-10-18 12:43:45 +0700

    constant

---
+++ belajar-golang-dasar/constant.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Eko"
+	const lastName = "Khannedy"
+	const value = 100
+
+	const (
+		age     = 20
+		address = "Bandung"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+	fmt.Println(age)
+}
