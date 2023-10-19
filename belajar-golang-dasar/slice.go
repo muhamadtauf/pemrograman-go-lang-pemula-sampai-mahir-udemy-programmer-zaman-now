4f8593261b1a2d066129d1e943224ba5356cbba9
Muhammad Taufik <muhamadtauf@gmail.com>
2023-10-19 15:49:49 +0700

    slice

---
+++ belajar-golang-dasar/slice.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func main() {
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+
+	var slice1 = months[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	//slice mengacu pada array, apabila array nya diubah, maka slice nya akan ikut berubah
+	//jika slice nya diubah, maka array nya akan berubah juga
+
+	//months[5] = "Diubah"
+	//fmt.Println(slice1)
+
+	//slice1[0] = "Mei Lagi"
+	//fmt.Println(months)
+
+	var slice2 = months[10:]
+	fmt.Println(slice2)
+
+	//slice akan membuat array baru jika kapasitasnya kurang
+	var slice3 = append(slice2, "Eko")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan Desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(months)
+
+	newSlice := make([]string, 2, 5)
+
+	newSlice[0] = "Eko"
+	newSlice[1] = "Kurniawan"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	//apabila kapasitas dari slice baru kurang, maka datanya akan tepotong
+	copySlice := make([]string, len(newSlice), cap(newSlice))
+	copy(copySlice, newSlice)
+	fmt.Println(copySlice)
+
+	iniArray := [5]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
+}
