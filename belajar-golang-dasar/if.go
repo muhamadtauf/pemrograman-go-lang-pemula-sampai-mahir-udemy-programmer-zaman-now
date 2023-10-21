6be56b1ca4a091e0e2f973ac4fbc51eaacaea80a
Muhammad Taufik <muhamadtauf@gmail.com>
2023-10-22 00:05:16 +0700

    if

---
+++ belajar-golang-dasar/if.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name = "Joko"
+
+	if name == "Eko" {
+		fmt.Println("Hello Eko")
+	} else if name == "Joko" {
+		fmt.Println("Hello Joko")
+	} else if name == "Budi" {
+		fmt.Println("Hello Budi")
+	} else {
+		fmt.Println("Hi, Kenalan dong")
+	}
+
+	// if short statement
+	if length := len(name); length > 5 {
+		fmt.Println("Terlalu Panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
+
+}
