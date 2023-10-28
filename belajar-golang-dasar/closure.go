ae2c325bfad46361a65bedccc66ecc3dae2b519e
Muhammad Taufik <muhamadtauf@gmail.com>
2023-10-28 18:08:10 +0700

    closure

---
+++ belajar-golang-dasar/closure.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Eko"
+	counter := 0
+
+	increment := func() {
+		name = "Budi"
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+
+	fmt.Println(name)
+	fmt.Println(counter)
+}
