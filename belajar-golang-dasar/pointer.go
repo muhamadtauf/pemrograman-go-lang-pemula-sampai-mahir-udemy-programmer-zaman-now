4c6f52073edf699a2a430ef3f335e265242133d0
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-03 23:51:29 +0700

    pointer

---
+++ belajar-golang-dasar/pointer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	address1 := Address{
+		City:     "Subang",
+		Province: "Jawa Barat",
+		Country:  "Indonesia",
+	}
+	address2 := address1
+
+	address2.City = "Bandung"
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+}
