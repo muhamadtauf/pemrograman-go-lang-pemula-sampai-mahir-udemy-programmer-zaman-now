511b44471f4647e2b7e62c1cec94855fdc4307e1
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-04 21:46:58 +0700

    pointer

--- belajar-golang-dasar/pointer.go
+++ belajar-golang-dasar/pointer.go
@@ -18,4 +18,63 @@ func main() {
 
 	fmt.Println(address1)
 	fmt.Println(address2)
+
+	address3 := Address{
+		City:     "Jakarta",
+		Province: "Jawa Barat",
+		Country:  "Indonesia",
+	}
+
+	//pointer
+	address4 := &address3
+
+	address4.City = "Cimahi"
+
+	fmt.Println(address3)
+	fmt.Println(address4)
+
+	///
+
+	address5 := Address{
+		City:     "Jakarta",
+		Province: "Jawa Barat",
+		Country:  "Indonesia",
+	}
+
+	address6 := &address5
+
+	address6.City = "Cimahi"
+	address6 = &Address{
+		City:     "Malang",
+		Province: "Jawa Timur",
+		Country:  "Indonesia",
+	}
+
+	fmt.Println(address5)
+	fmt.Println(address6)
+
+	///
+	address7 := Address{
+		City:     "Jakarta",
+		Province: "Jawa Barat",
+		Country:  "Indonesia",
+	}
+
+	address8 := &address7
+	address9 := &address7
+
+	address8.City = "Cimahi"
+	*address8 = Address{
+		City:     "Malang",
+		Province: "Jawa Timur",
+		Country:  "Indonesia",
+	}
+
+	fmt.Println(address7)
+	fmt.Println(address8)
+	fmt.Println(address9)
+
+	var address10 *Address = new(Address)
+	address10.City = "Bandung"
+	fmt.Println(address10)
 }
