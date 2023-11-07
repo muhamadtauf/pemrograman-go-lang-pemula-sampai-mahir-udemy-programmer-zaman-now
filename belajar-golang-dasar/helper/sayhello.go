addcd42c8a28f63100637304febd65c945bb53c1
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-07 22:59:23 +0700

    access modifier

--- belajar-golang-dasar/helper/sayhello.go
+++ belajar-golang-dasar/helper/sayhello.go
@@ -2,6 +2,23 @@ package helper
 
 import "fmt"
 
+// diawali dengan huruf kecil, hanya bisa diakses di package yang sama
+var version = "1.0.0"
+
+// diawali dengan huruf besar, bisa diakses dari package lain
+var Application = "Golang"
+
+// diawali dengan huruf kecil, hanya bisa diakses di package yang sama
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
+// diawali dengan huruf besar, bisa diakses dari package lain
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func Contoh() {
+	sayGoodBye("Eko")
+	fmt.Println(version)
 }
--- belajar-golang-dasar/import.go
+++ belajar-golang-dasar/import.go
@@ -1,7 +1,11 @@
 package main
 
-import "pemrograman-go-lang-pemula-sampai-mahir-udemy-programmer-zaman-now/belajar-golang-dasar/helper"
+import (
+	"fmt"
+	"pemrograman-go-lang-pemula-sampai-mahir-udemy-programmer-zaman-now/belajar-golang-dasar/helper"
+)
 
 func main() {
 	helper.SayHello("Eko")
+	fmt.Println(helper.Application)
 }
